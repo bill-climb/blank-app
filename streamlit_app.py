1550017892498e222ea7c99578a1fd1130a68120
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 13:44:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,8 @@ import pandas as pd
 import math
 from pathlib import Path
 
-@st.cache_data
-def get_ukc_data():
-    """Grab ukc data from a xlsx file.
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/logbook.xlsx'
-    df = pd.read_excel(DATA_FILENAME)
-    df[['style','style category']] = df['Style'].str.split(' ',expand=True)
-    df=df.drop(columns=['Style'])
-    df.rename(columns={'Partner(s)': 'Partner'}, inplace=True)
-    #add index
-    df['log_id'] = df.index + 1
-    #convert date
-    df['Date']=pd.to_datetime(df['Date'], format='%d/%b/%y')
-    #only show rows from this year
-    df=df[(df['Date'] > start_date)]
-    df["first star"]= df["Grade"].str.find('*')
-    df[['overall grade','technical grade', 'star rating']] = df['Grade'].str.split(' ',expand=True)
-
-    return df
-
-ukc_df = get_ukc_data()
-
-
+df = pd.read_excel('logbook.xlsx')
 
 st.title("🎈 My new app")
 st.write(
@@ -40,4 +13,4 @@ st.write(
 
 ''
 
-st.table(ukc_df)
+st.table(df)
